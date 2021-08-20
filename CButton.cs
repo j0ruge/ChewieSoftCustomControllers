b4257d9f07c4171445b29627f09a68c69cdcfd58
j0ruge <46424602+j0ruge@users.noreply.github.com>
2021-08-19 23:51:01 -0300

    Problems on CButton

--- CButton.cs
+++ CButton.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CustomController
 {
-    class CButton : Button
+    public class CButton : Button
     {
         #region => Fields
 
