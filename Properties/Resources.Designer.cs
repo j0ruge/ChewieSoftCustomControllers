1c676321a717cc1c5703bae7337eb64c14612b8b
j0ruge <46424602+j0ruge@users.noreply.github.com>
2021-08-24 21:08:41 -0300

    ChewieSoftCustomControllers.1.0.8

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace CustomController.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap chewieapp_logo {
+            get {
+                object obj = ResourceManager.GetObject("chewieapp_logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap userInputField {
             get {
                 object obj = ResourceManager.GetObject("userInputField", resourceCulture);
